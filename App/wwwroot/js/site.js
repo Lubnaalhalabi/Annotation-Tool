13c93d0f7e728af4ee6b7b9d24bf9718f2e5d033
Lubna Alhalabi <113240883+Lubnaalhalabi@users.noreply.github.com>
2024-09-06 17:13:27 +0300

    Add files via upload

---
+++ App/wwwroot/js/site.js
@@ -0,0 +1,47 @@
+﻿function Response(data) {
+    if (data['validationError'] == true) {
+        var message = '<ul>';
+        data['message'].forEach(function (element) {
+            element.errors.forEach(function (error) {
+                message += '<li>' + error.errorMessage + '</li>';
+            });
+        });
+        message += '</ul>';
+        Swal.fire({
+            title: 'Warning!',
+            html: message,
+            icon: 'warning',
+            confirmButtonColor: 'rgb(150, 41, 65)',
+            confirmButtonText: 'Ok',
+        });
+    }
+    else if (data['error'] == true) {
+        Swal.fire({
+            title: 'Warning!',
+            html: data['message'],
+            icon: 'warning',
+            confirmButtonColor: 'rgb(150, 41, 65)',
+            confirmButtonText: 'Ok',
+        });
+    }
+    else {
+        Swal.fire({
+            title: 'Success',
+            html: data['message'],
+            confirmButtonColor: 'rgb(150, 41, 65)',
+            confirmButtonText: 'Ok',
+        });
+        loadData();
+    }
+}
+
+setInterval(function () {
+    $.ajax({
+        url: '/Accountant/UpdateLastSeen',
+        type: 'POST',
+        dataType: 'json',
+        success: function (response) {
+            console.log("updated login " + response);
+        }
+    });
+},5000)
